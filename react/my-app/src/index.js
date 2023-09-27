f1a757b9c601ec5139f791aebf1cd8de20fb9621
kiya-moon <kiyamoon@daum.net>
2023-09-27 09:23:24 +0900

    react_study_2

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -6,14 +6,24 @@ import reportWebVitals from './reportWebVitals';
 
 // import 추가
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
 
-// 왜 선생님이랑 다르지...? 버전이 달라서인가...? >>> ㅇㅇ
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1000);
+
+// // 왜 선생님이랑 다르지...? 버전이 달라서인가...? >>> ㅇㅇ
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>
+// );
 
 // 이거는 실행이 안됨 >>> 현재 리액트는 18버전 이상이고 아래 코드는 이전 버전의 리액트에서 실행하던 방식
 // ReactDOM.render(
